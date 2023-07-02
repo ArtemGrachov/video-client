908929b5682be69219e8f5530189226b4f658e41
Artem Grachov <grachov2195@gmail.com>
2023-07-02 20:12:40 +0300

    Logout button

--- src/app/views/view-profile/view-profile.component.ts
+++ src/app/views/view-profile/view-profile.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
+import { AuthService } from 'src/app/modules/data/auth/services/auth.service';
 import { UserDataService } from 'src/app/modules/data/user-data/services/user-data.service';
 
 import { IUser } from 'src/app/types/models/user.interface';
@@ -12,7 +14,16 @@ import { IUser } from 'src/app/types/models/user.interface';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ViewProfileComponent {
-  constructor(private userDataService: UserDataService) {}
+  constructor(
+    private authService: AuthService,
+    private userDataService: UserDataService,
+    private router: Router,
+  ) {}
 
   public user$: Observable<IUser | null> = this.userDataService.data$;
+
+  public logoutHandler(): void {
+    this.authService.unauthorize();
+    this.router.navigate(['/']);
+  }
 }
