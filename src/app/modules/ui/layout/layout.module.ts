570fc7e561e64eea2b847e139ca58c4d95bb4d80
Artem Grachov <grachov2195@gmail.com>
2023-07-16 15:18:11 +0300

    Redesign

--- src/app/modules/ui/layout/layout.module.ts
+++ src/app/modules/ui/layout/layout.module.ts
@@ -3,20 +3,25 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { FormSearchModule } from '../forms/form-search/form-search.module';
+import { LogoModule } from '../other/logo/logo.module';
 
 import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
+    NavComponent,
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    NavComponent,
   ],
   imports: [
     CommonModule,
     RouterModule,
     FormSearchModule,
+    LogoModule,
   ]
 })
 export class LayoutModule { }
